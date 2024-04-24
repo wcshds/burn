028df64d73423dbea77dfb03f3aa97dc9a88dfc0
wcshds <wcshds@gmail.com>
2024-04-24 20:15:54 +0800

    fix conflict

--- crates/burn-core/src/nn/loss/mod.rs
+++ crates/burn-core/src/nn/loss/mod.rs
@@ -1,12 +1,13 @@
 mod binary_cross_entropy;
 mod cross_entropy;
+mod ctc;
 mod huber;
 mod mse;
 mod reduction;
 
 pub use binary_cross_entropy::*;
 pub use cross_entropy::*;
+pub use ctc::*;
 pub use huber::*;
 pub use mse::*;
 pub use reduction::*;
-
